4256722beadf58c1f852a5a8c14adf8df6e19555
espada <yua2479@gmail.com>
2024-12-19 21:39:23 +0900

    update client.js

--- client.js
+++ client.js
@@ -13,3 +13,22 @@ const requestData = {
 
 const methodList = ["floor", "nroot", "reverse", "validAnagram", "sort"];
 
+function question(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise((resolve) => {
+        rl.question(query, (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+
+function isNumericString(str) {
+    const num = Number(str);
+    return !isNaN(num) && str === num.toString();
+}
+
