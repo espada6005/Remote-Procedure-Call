0842bdf4c946903454cc644ae045dcf6a60c784b
espada <yua2479@gmail.com>
2024-12-20 22:11:27 +0900

    add server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+import os
+import json
+
+class Caliculation:
+
+    def floor(x):
+        return int(x // 1)
+    
+    def nroot(n, x):
+        return x ** (1 / n)
+    
+    def reserve(s):
+        return s[::-1]
+    
+    def validAnagram(str1, str2):
+        return sorted(str1) == sorted(str2)
+    
+    def sort(strArr):
+        return sorted(strArr)
